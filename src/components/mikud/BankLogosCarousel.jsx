a2a3c2b518b41e5f813fc2b07ba4bc9481580842
מיקוד משכנתאות <office@mikud4me.co.il>
2026-01-23 21:14:37 +0000

    File changes

--- src/components/mikud/BankLogosCarousel.jsx
+++ src/components/mikud/BankLogosCarousel.jsx
@@ -30,10 +30,10 @@ export default function BankLogosCarousel() {
             const getHeight = (bankName) => {
               switch(bankName) {
                 case 'לאומי': return '28px';
-                case 'הפועלים': return '84px';
+                case 'הפועלים': return '110px';
                 case 'דיסקונט': return '38px';
-                case 'מזרחי טפחות': return '84px';
-                case 'ירושלים': return '84px';
+                case 'מזרחי טפחות': return '110px';
+                case 'ירושלים': return '110px';
                 case 'מרכנתיל': return '38px';
                 default: return '38px';
               }
